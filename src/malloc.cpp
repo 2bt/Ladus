4ef1d9de5cc54084c897ac55610db0dc13ed6ea1
2bt <s8572327@gmail.com>
2021-02-15 01:39:31 +0100

    added license to malloc

--- src/malloc.cpp
+++ src/malloc.cpp
@@ -1,7 +1,36 @@
+/*
+    NOTE: I took this code from
+    https://github.com/maxkl/wasm-malloc/blob/master/src/malloc.c
+    and modified it a bit.
+
+    MIT License
+
+    Copyright (c) 2019 Max Klein
+
+    Permission is hereby granted, free of charge, to any person obtaining a copy
+    of this software and associated documentation files (the "Software"), to deal
+    in the Software without restriction, including without limitation the rights
+    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+    copies of the Software, and to permit persons to whom the Software is
+    furnished to do so, subject to the following conditions:
+
+    The above copyright notice and this permission notice shall be included in all
+    copies or substantial portions of the Software.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+    SOFTWARE.
+*/
+
+
 #include "stdwasm.hpp"
 
 #define PAGE_SIZE (64 * 1024)
-
+#define BLOCK_INFO_SIZE (sizeof(block_info))
 
 struct block_info {
     block_info* previous;
@@ -9,7 +38,6 @@ struct block_info {
     size_t      size;
     bool        free;
 };
-#define BLOCK_INFO_SIZE (sizeof(block_info))
 
 static bool        initialized = false;
 static size_t      current_pages;
